54ca7b97f5fbcce7c6b15a52da696765665c8712
Hemanth S A <hemanth_sa@intuit.com>
2016-06-16 11:07:43 -0700

    Updated schema

--- Schema.sql
+++ Schema.sql
@@ -23,8 +23,24 @@ CREATE TABLE Expenses (
     credit_debit int NOT NULL,
     transaction_name varchar(200) DEFAULT 'Unknown',
     description varchar(1000) DEFAULT NULL,
+    total_remaining double NOT NULL,
     user int NOT NULL,
     FOREIGN KEY(user) REFERENCES User(id) ON UPDATE cascade ON DELETE no action,
     type int,
     FOREIGN KEY(type) REFERENCES ExpenseType(id) ON UPDATE cascade ON DELETE no action
 );
+
+CREATE TABLE Credits (
+    id bigint PRIMARY KEY AUTO_INCREMENT,
+    amount double NOT NULL,
+    date datetime NOT NULL DEFAULT NOW(),
+    credit bigint NOT NULL,
+    FOREIGN KEY(credit) REFERENCES Expenses(id) ON UPDATE cascade ON DELETE cascade,
+    credit_on bigint NOT NULL,
+    FOREIGN KEY(credit_on) REFERENCES Expenses(id) ON UPDATE cascade ON DELETE cascade
+);
+
+CREATE TRIGGER total_remaining_in_expenses BEFORE INSERT ON Expenses FOR EACH ROW
+    IF NEW.total_remaining IS NULL THEN
+        SET NEW.total_remaining := NEW.amount;
+    END IF;;
